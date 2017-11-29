73a9b0dc3a8640cf216d1a1b01ebd3cc7bc31134
Mayur <mayur@MacBook-Pro-van-Pankaj.local>
2017-11-29 13:18:17 +0530

    First commit

---
+++ src/views/app/app.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import TasksPage from '../pages/tasks';
+
+
+const App = () => (
+  <div>
+    <main>
+      <TasksPage />
+    </main>
+  </div>
+);
+
+//=====================================
+//  CONNECT
+//-------------------------------------
+
+
+export default withRouter(
+  connect(
+  )(App)
+);
---
+++ src/views/pages/tasks/tasks-page.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import { List } from 'immutable';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
+
+import { getTaskFilter, getVisibleTasks, tasksActions } from 'src/tasks';
+import TaskForm from '../../components/task-form';
+import TaskList from '../../components/task-list';
+
+
+export class TasksPage extends Component {
+  static propTypes = {
+    createTask: PropTypes.func.isRequired,
+    filterTasks: PropTypes.func.isRequired,
+    loadTasks: PropTypes.func.isRequired,
+    removeTask: PropTypes.func.isRequired,
+    tasks: PropTypes.instanceOf(List).isRequired,
+    undeleteTask: PropTypes.func.isRequired,
+    unloadTasks: PropTypes.func.isRequired,
+    updateTask: PropTypes.func.isRequired
+  };
+
+  componentWillMount() {
+    this.props.loadTasks();
+  }
+
+  componentWillUnmount() {
+    this.props.unloadTasks();
+  }
+
+  render() {
+    return (
+      <div className="g-row">
+        <div className="g-col">
+          <TaskForm handleSubmit={this.props.createTask} />
+        </div>
+
+        <div className="g-col">
+          <TaskList
+            removeTask={this.props.removeTask}
+            tasks={this.props.tasks}
+            updateTask={this.props.updateTask}
+          />
+        </div>
+
+      </div>
+    );
+  }
+}
+
+
+//=====================================
+//  CONNECT
+//-------------------------------------
+
+const mapStateToProps = createSelector(
+  getTaskFilter,
+  getVisibleTasks,
+  (filterType, tasks) => ({
+    filterType,
+    tasks
+  })
+);
+
+const mapDispatchToProps = Object.assign(
+  {},
+  tasksActions,
+);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TasksPage);
