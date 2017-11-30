4335402c4be173288904aaa73828a8e2afc7f512
Mayur <mayur@MacBook-Pro-van-Pankaj.local>
2017-11-30 21:18:34 +0530

    Fixed bugs

--- src/views/components/task-list/task-list.js
+++ src/views/components/task-list/task-list.js
@@ -6,54 +6,57 @@ import {SortableContainer, SortableElement} from 'react-sortable-hoc';
 
 
 function TaskList({removeTask, tasks, updateTask, updateTaskOrdering}) {
-    const taskItems = tasks.map((task, index) => {
-        return (
-            <TaskItem
-                key={index}
-                taskKey={index}
-                task={task}
-                removeTask={removeTask}
-                updateTask={updateTask}
-            />
-        );
-    });
-
-    const onSortEnd = ({oldIndex, newIndex}) => {
-        if (tasks.get(oldIndex) && tasks.get(newIndex)) {
-            return;
-        }
-        updateTaskOrdering({sourceTask: tasks.get(oldIndex).toJS(), targetTask: tasks.get(newIndex).toJS(),
-            sourceIndex: oldIndex, targetIndex: newIndex});
-    };
-
+  const taskItems = tasks.map((task, index) => {
     return (
-        <div className="task-list">
-            <SortableList distance={1} lockAxis={'y'} taskItems={taskItems} onSortEnd={onSortEnd}/>
-        </div>
+      <TaskItem
+        key={index}
+        taskKey={index}
+        task={task}
+        removeTask={removeTask}
+        updateTask={updateTask}
+      />
     );
+  });
+
+  const onSortEnd = ({oldIndex, newIndex}) => {
+    if (!tasks.get(oldIndex) && !tasks.get(newIndex)) {
+      return;
+    }
+
+    updateTaskOrdering({
+      sourceTask: tasks.get(oldIndex).toJS(), targetTask: tasks.get(newIndex).toJS(),
+      sourceIndex: oldIndex, targetIndex: newIndex
+    });
+  };
+
+  return (
+    <div className="task-list">
+      <SortableList distance={1} lockAxis={'y'} taskItems={taskItems} onSortEnd={onSortEnd}/>
+    </div>
+  );
 }
 
 
 const SortableItem = SortableElement(({value}) =>
-    <li className="listWrapperSortable">{value}</li>
+  <li className="listWrapperSortable">{value}</li>
 );
 
 const SortableList = SortableContainer(({taskItems}) => {
-    return (
-        <ul>
-            {taskItems.map((task, index) => (
-                <SortableItem key={`item-${index}`} index={index} value={task}/>
-            ))}
-        </ul>
-    );
+  return (
+    <ul>
+      {taskItems.map((task, index) => (
+        <SortableItem key={`item-${index}`} index={index} value={task}/>
+      ))}
+    </ul>
+  );
 });
 
 
 TaskList.propTypes = {
-    removeTask: PropTypes.func.isRequired,
-    tasks: PropTypes.instanceOf(List).isRequired,
-    updateTask: PropTypes.func.isRequired,
-    updateTaskOrdering: PropTypes.func.isRequired
+  removeTask: PropTypes.func.isRequired,
+  tasks: PropTypes.instanceOf(List).isRequired,
+  updateTask: PropTypes.func.isRequired,
+  updateTaskOrdering: PropTypes.func.isRequired
 };
 
 export default TaskList;
